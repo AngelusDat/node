9076b91adbe2e21aa3b0e68ca6ee0addba377f77
JuanHeza <juanehza@hotmail.com>
2023-06-20 17:25:10 +0000

    inital

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+    res.send('Hello Express app!')
 });
 
 app.listen(3000, () => {
-  console.log('server started');
+    console.log('server started');
 });
+///////////
